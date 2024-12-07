394f75f6fc694fab1b30670a44e6a58087ce9f17
Rohan Saxena <rsrohansaxena903@gmail.com>
2024-12-07 09:08:20 +0530

    Initial commit

---
+++ src/main/java/com/apicaller/trub/TestRouteUpdateBrigdeApplication.java
@@ -0,0 +1,13 @@
+package com.apicaller.trub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRouteUpdateBrigdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestRouteUpdateBrigdeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apicaller/trub/TestRouteUpdateBrigdeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apicaller.trub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRouteUpdateBrigdeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
